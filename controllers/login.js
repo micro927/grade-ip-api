3008afd39b98552e792eddbc4b53546d6c3b163d
micro927 <nsbelieve-15@hotmail.com>
2022-07-26 18:03:03 +0700

    devdevdev

--- controllers/login.js
+++ controllers/login.js
@@ -1,21 +1,22 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const qcode = req.body.code;
-// const qstate = req.body.state;
-// const qline = req.body.line || false;
+export const login = (req, res) = {
+    const qcode = req.body.code;
+    // const qstate = req.body.state;
+    // const qline = req.body.line || false;
 
-axios({
-    method: "post",
-    url: "https://oauth.cmu.ac.th/v1/GetToken.aspx",
-    timeout: 4000, // 4 seconds timeout
-    params: {
-        code: req.body.code,
-        redirect_uri: process.env.APP_REDIRECT,
-        client_id: process.env.OAUTH_CLIENT_ID,
-        client_secret: process.env.OAUTH_CLIENT_SECRET,
-        grant_type: "authorization_code",
-    },
+    axios({
+        method: "post",
+        url: "https://oauth.cmu.ac.th/v1/GetToken.aspx",
+        timeout: 4000, // 4 seconds timeout
+        params: {
+            code: req.body.code,
+    redirect_uri: process.env.APP_REDIRECT,
+    client_id: process.env.OAUTH_CLIENT_ID,
+    client_secret: process.env.OAUTH_CLIENT_SECRET,
+    grant_type: "authorization_code",
+},
 }).then((response) => {
     axios({
         method: "get",
@@ -32,3 +33,4 @@ axios({
     .catch((error) => {
         console.error(error);
     });
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import * as dotenv from 'dotenv'
 import courses from './routes/courses.js'
+import login from './routes/login.js'
 
 dotenv.config()
 const app = express()
@@ -11,6 +12,7 @@ app.use(bodyParser.json())
 app.get("/", (req, res) => res.send("Hello, World!"))
 
 app.use('/courses', courses)
+app.use('/login', login)
 
 app.listen((process.env.HOST, process.env.PORT), () => {
     console.log(`Running on http://${process.env.HOST}:${process.env.PORT}`);
