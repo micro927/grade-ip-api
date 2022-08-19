49bb755eb2cde811a55535abc2e71f2e22eba86e
Sitthiphon Skulphan <nsbelieve-15@hotmail.com>
2022-08-19 22:10:01 +0700

    dev: courselist

--- controllers/courseList.js
+++ controllers/courseList.js
@@ -5,17 +5,21 @@ dotenv.config()
 
 const courseList = async (req, res) => {
     const userInfo = res.locals.UserDecoded
+    const gradeType = req.query.gradeType
     const connection = await mysqlConnection('online_grade_ip')
     await connection.query(`SELECT * FROM
-                            (SELECT class_id, 'teach' class_type, semester,year,courseno,seclec,seclab,ip_type,yearly FROM tbl_class WHERE courseno IN (:courseno)
+                            (SELECT class_id, 'inList' class_type,semester,year,courseno,seclec,seclab,ip_type,yearly,bulletin_id FROM tbl_class WHERE courseno IN (:courseno)
                             UNION
-                            SELECT class_id, 'inList' class_type, semester,year,courseno,seclec,seclab,ip_type,yearly FROM tbl_class WHERE instructor_id IN(:instructorId)) main
+                            SELECT class_id, 'teach' class_type,semester,year,courseno,seclec,seclab,ip_type,yearly,bulletin_id FROM tbl_class WHERE instructor_id IN(:instructorId)) main
+                            LEFT JOIN (SELECT bulletin_id,TRIM(title_short_en) course_title FROM db_center.tbl_bulletin) bulletin
+                            USING(bulletin_id)
                             JOIN
                             (SELECT class_id,COUNT(*) all_student ,COUNT(CASE WHEN(grade_new IS NOT NULL) THEN 1 END) filled_student FROM tbl_student_grade GROUP BY class_id) student
-                            USING(class_id)`,
+                            USING(class_id) WHERE ip_type = :ip_type`,
         {
             courseno: userInfo.courseList,
-            instructorId: userInfo.instructorId
+            instructorId: userInfo.instructorId,
+            ip_type: gradeType
         }
     )
         .then(([rows]) => {
