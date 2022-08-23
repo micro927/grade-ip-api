f62cf7167c37a022e2574e13088ba3384ff81c92
micro927 <nsbelieve-15@hotmail.com>
2022-08-23 17:54:36 +0700

    add: coursedetail

---
+++ controllers/teacher/courseDetail.js
@@ -0,0 +1,30 @@
+import * as dotenv from 'dotenv'
+import mysqlConnection from '../../connection/mysql.js'
+dotenv.config()
+
+const courseDetail = async (req, res) => {
+    const userInfo = res.locals.UserDecoded
+    const classId = req.params.classId
+    const connection = await mysqlConnection('online_grade_ip')
+    await connection.query(`SELECT tbl_class.*,course_title 
+                            FROM tbl_class
+                            LEFT JOIN (SELECT courseno,bulletin_id,TRIM(title_short_en) course_title FROM db_center.tbl_bulletin) bulletin
+                            USING(courseno,bulletin_id) 
+                            WHERE class_id = :classId AND(tbl_class.courseno IN (:courseList) OR instructor_id IN(:instructorId)) LIMIT 1`,
+        {
+            classId: classId,
+            courseList: userInfo.courseList,
+            instructorId: userInfo.instructorId,
+        }
+    )
+        .then(([rows]) => {
+            if (rows.length > 0) {
+                res.status(200).json(rows[0])
+            }
+            else {
+                res.status(403).json([])
+            }
+        })
+}
+
+export default courseDetail
--- routes/teacher.js
+++ routes/teacher.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import courseList from '../controllers/teacher/courseList.js'
+import courseDetail from '../controllers/teacher/courseDetail.js'
 import studentGradeList from '../controllers/teacher/studentGradeList.js'
 
 const router = express.Router()
@@ -8,6 +9,7 @@ router.get('/', (req, res) => {
 })
 
 router.get('/courselist', courseList)
+router.get('/coursedetail/:classId', courseDetail)
 router.get('/fill/:classId', studentGradeList)
 
 export default router
