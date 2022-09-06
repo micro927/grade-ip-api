74f765a28379f3a44550d23c0b14f3c94ea28532
micro927 <nsbelieve-15@hotmail.com>
2022-09-06 17:39:59 +0700

    change: api result

--- controllers/teacher/studentGradeList.js
+++ controllers/teacher/studentGradeList.js
@@ -6,7 +6,14 @@ const studentGradeList = async (req, res) => {
     const userInfo = res.locals.UserDecoded
     const classId = req.params.classId
     const connection = await mysqlConnection('online_grade_ip')
-    await connection.query(`SELECT tbl_student_grade.*,TRIM(name_th) name,TRIM(surname_th) surname 
+    await connection.query(`SELECT student_id,
+                                    TRIM(name_th) name,
+                                    TRIM(surname_th) surname,
+                                    enroll_status,
+                                    grade_old,
+                                    grade_new,
+                                    fill_itaccountname,
+                                    fill_datetime
                             FROM tbl_student_grade
                             LEFT JOIN tbl_class USING(class_id)
                             LEFT JOIN db_center.tbl_student USING(student_id) 
@@ -18,8 +25,12 @@ const studentGradeList = async (req, res) => {
         }
     )
         .then(([rows]) => {
+            let inputGradeList = {}
             if (rows.length > 0) {
-                res.status(200).json(rows)
+                rows.map((row, index) => {
+                    inputGradeList[row.student_id] = row.grade_new ?? ''
+                })
+                res.status(200).json([rows, inputGradeList])
             }
             else {
                 res.status(403).json([])
@@ -27,7 +38,7 @@ const studentGradeList = async (req, res) => {
         })
         .catch((error) => {
             res.status(500)
-            console.log("mysqlConnection ERROR: ", error.code);
+            console.log("mysqlConnection ERROR: ", error);
         })
     await connection.end()
 
